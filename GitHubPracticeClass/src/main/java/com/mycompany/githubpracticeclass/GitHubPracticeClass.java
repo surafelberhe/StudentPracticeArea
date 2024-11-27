5e7e26d561ab35622a98cfc1eb40dba970feae56
ggreat <gebrehiwotreda@gmail.com>
2024-11-27 19:02:36 +0300

    first git method
    first git methods shows a welcome to github message.

---
+++ GitHubPracticeClass/src/main/java/com/mycompany/githubpracticeclass/GitHubPracticeClass.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.githubpracticeclass;
+
+/**
+ *
+ * @author g
+ */
+public class GitHubPracticeClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    public void firstGitMethod(){
+        System.out.println("Well Come to Github");
+    }
+}
